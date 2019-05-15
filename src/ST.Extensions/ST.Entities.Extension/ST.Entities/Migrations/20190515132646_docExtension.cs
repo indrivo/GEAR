dbf6ed059d212a63966902acc40f7107506d533f
SOFT-TEHNICA\Andrei.Culiuc <andrei.culiuc@soft-tehnica.com>
2019-05-15 21:03:39 +0300

    feat: Fake DMS

---
+++ src/ST.Extensions/ST.Entities.Extension/ST.Entities/Migrations/20190515132646_docExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ST.Entities.Migrations
+{
+    public partial class docExtension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Extension",
+                schema: "Entities",
+                table: "Documents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Extension",
+                schema: "Entities",
+                table: "Documents");
+        }
+    }
+}
--- src/ST.Extensions/ST.Entities.Extension/ST.Entities/Migrations/20190515150259_docstatus.Designer.cs
+++ src/ST.Extensions/ST.Entities.Extension/ST.Entities/Migrations/20190515150259_docstatus.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ST.Entities.Data;
@@ -9,9 +10,10 @@ using ST.Entities.Data;
 namespace ST.Entities.Migrations
 {
     [DbContext(typeof(EntitiesDbContext))]
-    partial class EntitiesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190515150259_docstatus")]
+    partial class docstatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
