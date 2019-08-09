82c6a5de129461c48125a9bc3f3959bdca9e1e81
Lupei Nicolae <nicolae.lupei@soft-tehnica.com>
2019-08-09 22:35:34 +0300

    fix: Report context factory

---
+++ src/ST.Extensions/ST.Report.Extension/ST.Report.Dynamic/Data/ReportDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using ST.Core.Helpers.DbContexts;
+
+namespace ST.Report.Dynamic.Data
+{
+    public class ReportDbContextFactory : IDesignTimeDbContextFactory<DynamicReportDbContext>
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// For creating migrations
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public DynamicReportDbContext CreateDbContext(string[] args)
+        {
+            return DbContextFactory<DynamicReportDbContext, DynamicReportDbContext>.CreateFactoryDbContext();
+        }
+    }
+}
