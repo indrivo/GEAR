badcb7864a7e0a12c2f2ac3b11ac7104a1eeaaa8
Cristian Nirca <cristian.nirca@indrivo.com>
2019-08-20 10:51:17 +0300

    feat: translation

--- src/ST.WebHosts/ST.Cms/wwwroot/themes/iso/js/meetings.js
+++ src/ST.WebHosts/ST.Cms/wwwroot/themes/iso/js/meetings.js
@@ -4,14 +4,14 @@ const db = new DataInjector();
 const toast = new ToastNotifier();
 const templateManager = new TemplateManager();
 const helper = new ST();
-
 //window.translate("key");
 templateManager.registerTemplate("template_template_meeting_children_accordion");
 $(() => {
-	const meetingId = helper.getParamFormUrl("meetingId");
+	const meetingId = "79155b6a-a187-43f8-b087-374384367b12"; //helper.getParamFormUrl("meetingId");
 	if (!meetingId) return toast.notify({ heading: "Meeting not found" });
 	db.getByIdWithIncludesAsync("Meeting", meetingId)
 		.then(dbResult => {
+			console.log(dbResult);
 			if (dbResult.is_success) {
 				const filters = [{ parameter: "MeetingId", value: meetingId }];
 				db.getAllWhereWithIncludesAsync("MeetingParticipant", filters).then(dbParticipants => {
@@ -38,29 +38,4 @@ $(() => {
 		}).catch(err => {
 			toast.notify({ heading: "Fail" });
 		});
-});
-
-$(() => {
-	var accordionItems = [
-		{
-			id: helper.newGuid(),
-			name: window.translate("iso_meeting_action_state"),
-			items: [
-				{
-					name: "Rezultatul analizei de management",
-					value: "content"
-				}
-			]
-		},
-		{
-			id: helper.newGuid(),
-			name: "Schimbarile aspectelor interne si extreme relevante pentru sistemul de management",
-			items: [
-				{
-					name: ""
-				}
-			]
-		}
-	];
-	console.log(accordionItems);
 });
