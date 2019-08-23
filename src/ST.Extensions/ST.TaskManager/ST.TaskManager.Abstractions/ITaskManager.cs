8088a9a241a496bcf2e1497211ebfc20a99fa4b5
plescacristian <plesca.cristiann@gmail.com>
2019-08-23 13:50:49 +0300

    feat: task manager abstraction

--- src/ST.Extensions/ST.TaskManager/ST.TaskManager.Abstractions/ITaskManager.cs
+++ src/ST.Extensions/ST.TaskManager/ST.TaskManager.Abstractions/ITaskManager.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using ST.Core.Helpers;
+using ST.TaskManager.Abstractions.Models.ViewModels;
 
 namespace ST.TaskManager.Abstractions
 {
-    public interface ITaskManager<TUser> where TUser : IdentityUser
+    public interface ITaskManager //<in TUser> where TUser : IdentityUser
     {
-        Task<ResultModel> CreateTaskAsync(TUser user);
+        Task<ResultModel<GetTaskViewModel>> GetTaskAsync(Guid taskId);
+        Task<ResultModel<List<TaskItemViewModel>>> GetTaskItemsAsync(Guid taskId);
+        Task<ResultModel<List<GetTaskViewModel>>> GetTasksAsync(Guid userId);
+        Task<ResultModel<Guid>> CreateTaskAsync(CreateTaskViewModel task);
+        Task<ResultModel> UpdateTaskAsync(UpdateTaskViewModel task);
+        Task<ResultModel> DeleteTaskAsync(Guid taskId);
+        Task<ResultModel> DeletePermanentTaskAsync(Guid taskId);
+        Task<ResultModel<Guid>> CreateTaskItemAsync(TaskItemViewModel task);
+        Task<ResultModel<Guid>> UpdateTaskItemAsync(TaskItemViewModel task);
+        Task<ResultModel> DeleteTaskItemAsync(Guid taskItemId);
     }
 }
--- src/ST.Extensions/ST.TaskManager/ST.TaskManager.Abstractions/Models/Task.cs
+++ src/ST.Extensions/ST.TaskManager/ST.TaskManager.Abstractions/Models/Task.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ST.Core;
+using ST.Identity.Abstractions;
+using ST.TaskManager.Abstractions.Enums;
 
 namespace ST.TaskManager.Abstractions.Models
 {
@@ -12,6 +15,19 @@ namespace ST.TaskManager.Abstractions.Models
         [Required]
         public string Name { get; set; }
 
+        [Required]
+        public string Description { get; set; }
+
         public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public TaskPriority TaskPriority { get; set; }
+
+        public TaskStatus Status { get; set; }
+
+        public virtual ICollection<TaskItem> TaskItems { get; set; }
     }
 }
