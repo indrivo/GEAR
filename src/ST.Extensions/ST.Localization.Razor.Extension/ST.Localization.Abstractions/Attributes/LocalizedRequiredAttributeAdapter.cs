be8ac7854863e2f7cd1d0ad463e5125168a932cc
Lupei Nicolae <nicolae.lupei@indrivo.com>
2019-08-27 15:56:49 +0300

    beta: required message translate

--- src/ST.Extensions/ST.Localization.Razor.Extension/ST.Localization.Abstractions/Attributes/LocalizedRequiredAttributeAdapter.cs
+++ src/ST.Extensions/ST.Localization.Razor.Extension/ST.Localization.Abstractions/Attributes/LocalizedRequiredAttributeAdapter.cs
@@ -3,13 +3,19 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.DataAnnotations.Internal;
 using Microsoft.Extensions.Localization;
+using ST.Core.Attributes;
+using ST.Core.Helpers;
 
 namespace ST.Localization.Abstractions.Attributes
 {
     public class LocalizedRequiredAttributeAdapter : RequiredAttributeAdapter
     {
-        public LocalizedRequiredAttributeAdapter(RequiredAttribute attribute, IStringLocalizer stringLocalizer) : base(attribute, stringLocalizer)
+        public LocalizedRequiredAttributeAdapter(RequiredAttribute attribute, IStringLocalizer stringLocalizer) : base(attribute, stringLocalizer) { }
+
+        public LocalizedRequiredAttributeAdapter(RequiredTranslateAttribute attribute, IStringLocalizer stringLocalizer) : base(attribute, stringLocalizer)
         {
+            var translated = stringLocalizer[attribute.Key].Value;
+            attribute.ErrorMessage = attribute.FormatErrorMessage("{0} " + translated);
 
         }
     }
@@ -18,6 +24,7 @@ namespace ST.Localization.Abstractions.Attributes
     {
         public virtual IAttributeAdapter GetAttributeAdapter(ValidationAttribute attribute, IStringLocalizer stringLocalizer)
         {
+            var localizer = IoC.Resolve<IStringLocalizer>();
             if (attribute == null)
             {
                 throw new ArgumentNullException(nameof(attribute));
@@ -29,7 +36,12 @@ namespace ST.Localization.Abstractions.Attributes
 
             if (type == typeof(RequiredAttribute))
             {
-                adapter = new LocalizedRequiredAttributeAdapter((RequiredAttribute)attribute, stringLocalizer);
+                adapter = new LocalizedRequiredAttributeAdapter((RequiredAttribute)attribute, localizer);
+            }
+
+            if (type == typeof(RequiredTranslateAttribute))
+            {
+                adapter = new LocalizedRequiredAttributeAdapter((RequiredTranslateAttribute)attribute, localizer);
             }
 
             return adapter;
