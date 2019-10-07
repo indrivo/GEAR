ff2ed1775e82178d946ea2af7348baeb3ff371e3
Mr Kid <nicolae.girbu@indrivo.com>
2019-10-07 16:43:23 +0300

    Calendar files and task manager fixes

---
+++ src/ST.Extensions/ST.Calendar.Extensions/ST.Calendar.Abstraction/Models/ViewModels/BaseEventViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ST.Calendar.Abstractions.Enums;
+
+namespace ST.Calendar.Abstractions.Models.ViewModels
+{
+    public class BaseEventViewModel : IValidatableObject
+    {
+        /// <summary>
+        /// Event name
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public virtual string Title { get; set; }
+
+        /// <summary>
+        /// Event description
+        /// </summary>
+        public virtual string Details { get; set; }
+
+        /// <summary>
+        /// Event location
+        /// </summary>
+        public virtual string Location { get; set; }
+
+        /// <summary>
+        /// Start date
+        /// </summary>
+        [Required]
+        public virtual DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// End date
+        /// </summary>
+        [Required]
+        public virtual DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Event priority
+        /// </summary>
+        public virtual EventPriority Priority { get; set; } = EventPriority.Low;
+
+        /// <summary>
+        /// Event member
+        /// </summary>
+        public virtual ICollection<Guid> Members { get; set; } = new List<Guid>();
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Custom validations
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (EndDate < StartDate)
+            {
+                yield return new ValidationResult("End Date must be greater than Start Date");
+            }
+            else if (StartDate < DateTime.Now)
+            {
+                yield return new ValidationResult("The event can only be created after this current time");
+            }
+        }
+    }
+}
