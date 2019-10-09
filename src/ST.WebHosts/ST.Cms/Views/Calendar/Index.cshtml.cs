ef97f9ca8c74ea2174f78dba78405671ca2520f4
Mr Kid <nicolae.girbu@indrivo.com>
2019-10-09 09:08:31 +0300

    merge calendar

---
+++ src/ST.WebHosts/ST.Cms/Views/Calendar/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ST.Calendar.Razor.Views.InternalCalendar
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
