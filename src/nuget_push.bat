cce3f74bb007d911137649ac7a341f35c4466ebf
Lupei Nicolae <nicolae.lupei@indriov.com>
2019-10-14 12:11:32 +0300

    feat: configure api key to push to nuget server

--- src/nuget_push.bat
+++ src/nuget_push.bat
@@ -1,10 +1,14 @@
-SET pushKey="ODExNDE3Mjk5NGVkNGMzZWI1MzNmZWU2NjhhNTI4MGE1MDZkODRmM2UwMWI0MjZmOTNiZWEzOTBkNTU2MDAxYw=="
-SET pushHost="http://192.168.1.70"
+SET pushKey="oy2cdyotfbd5rk7thnias2paqkriagtuxahd5t2n3woexy"
+SET pushHost="https://www.nuget.org"
 
 dotnet build ../ST.GEAR.sln
 
 dotnet pack ./ST.Extensions/ST.Core.Extension/ST.Core/ST.Core.csproj -o ../../../nupkgs
+dotnet pack ./ST.Extensions/ST.Cache.Extension/ST.Cache.Abstractions/ST.Cache.Abstractions.csproj -o ../../../nupkgs
 
+cd ./nupkgs
 
-rm ./*.nupkg
+dotnet nuget push -k %pushKey% -s %pushHost% ST.Core*
+dotnet nuget push -k %pushKey% -s %pushHost% ST.Cache.Abstractions*
+
 PAUSE
