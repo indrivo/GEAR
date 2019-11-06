91f70dfbebca098c431ab74c32f558050f9872e7
Ion Dorosenco <ion.dorosenco@soft-tehnica.com>
2019-11-06 12:20:39 +0200

    subscription repository

---
+++ src/GR.Extensions/GR.Ecommerce.Extension/GR.Subscription/GR.Subscription.Abstractions/ISubscriptionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GR.Core.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using GR.Subscription.Abstractions.Models;
+
+
+namespace GR.Subscription.Abstractions
+{
+    public interface ISubscriptionDbContext : IDbContext
+    {
+        /// <summary>
+        /// Subscription
+        /// </summary>
+        DbSet<Models.Subscription> Subscription { get; set; }
+    }
+}
---
+++ src/GR.Extensions/GR.Ecommerce.Extension/GR.Subscription/GR.Subscription.Abstractions/ViewModels/SubscriptionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GR.Subscription.Abstractions.ViewModels
+{
+    public class SubscriptionViewModel: Models.Subscription
+    {
+    }
+}
