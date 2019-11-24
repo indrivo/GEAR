dffd82562c98f0632658e101d76f0287e8297689
Lupei Nicolae <nicolae.lupei@indrivo.com>
2019-11-24 18:06:57 +0200

    feat: add universal currencies

---
+++ src/GR.Extensions/GR.Ecommerce.Extension/GR.ECommerce.Abstractions/Helpers/CommerceResources.cs
@@ -0,0 +1,12 @@
+﻿namespace GR.ECommerce.Abstractions.Helpers
+{
+    public static class CommerceResources
+    {
+        public static class SystemCurrencies
+        {
+            public const string MDL = "MDL";
+            public const string USD = "USD";
+            public const string EUR = "EUR";
+        }
+    }
+}
