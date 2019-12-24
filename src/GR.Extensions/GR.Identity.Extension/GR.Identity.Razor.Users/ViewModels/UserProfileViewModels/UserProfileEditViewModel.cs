ea7448420ec4261db2136641dcb6023c91fb2d00
Doinita <doinita@mail.com>
2019-12-24 12:10:11 +0200

    translations

--- src/GR.Extensions/GR.Identity.Extension/GR.Identity.Razor.Users/ViewModels/UserProfileViewModels/UserProfileEditViewModel.cs
+++ src/GR.Extensions/GR.Identity.Extension/GR.Identity.Razor.Users/ViewModels/UserProfileViewModels/UserProfileEditViewModel.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using GR.Core;
 
 namespace GR.Identity.Razor.Users.ViewModels.UserProfileViewModels
 {
     public class UserProfileEditViewModel
     {
         public string Id { get; set; }
-
+        
+        [Required]
         [MaxLength(50)]
         [Display(Name = "First name", Description = "first name", Prompt = "ex: John")]
-        public string UserFirstName { get; set; }
+        public virtual string UserFirstName { get; set; }
 
+        [Required]
         [MaxLength(50)]
         [Display(Name = "Last name", Description = "last name", Prompt = "ex: Doe")]
-        public string UserLastName { get; set; }
+        public virtual string UserLastName { get; set; }
 
         [MaxLength(20)]
         [Display(Name = "Phone number", Description = "phone number ", Prompt = "0123456789")]
         [DataType(DataType.PhoneNumber)]
-        public string UserPhoneNumber { get; set; }
+        public virtual string UserPhoneNumber { get; set; }
 
         [Display(Name = "Birthday", Description = "birthday")]
-        public DateTime Birthday { get; set; }
+        public virtual DateTime Birthday { get; set; } = DateTime.Now;
 
         [MaxLength(500)]
         [Display(Name = "About me", Description = "same description")]
-        public string AboutMe { get; set; }
+        public virtual string AboutMe { get; set; }
     }
 }
