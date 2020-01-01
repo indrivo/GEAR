bcc0bd3401ebc3caa15ef46d85a651687c19101a
nicolae <nicolae.lupei@indrivo.com>
2020-01-01 16:49:45 +0200

    feat: logs  runtime  view

--- src/GR.Extensions/GR.Logger.Extension/GR.Logger/GearLoggerTarget.cs
+++ src/GR.Extensions/GR.Logger.Extension/GR.Logger/GearLoggerTarget.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using GR.Core.Helpers;
+using GR.Logger.Abstractions.Helpers;
 using Microsoft.Extensions.Caching.Memory;
 using NLog;
 using NLog.Targets;
@@ -10,7 +11,6 @@ namespace GR.Logger
     [Target("GearTarget")]
     public class GearLoggerTarget : TargetWithLayout
     {
-
         #region Injectable
 
         private static IMemoryCache CacheService { get; set; }
@@ -30,12 +30,16 @@ namespace GR.Logger
         {
             var message = Layout.Render(logEvent);
             if (_cacheService == null) return;
-            var logsActivated = _cacheService.Get<bool?>("TempLogsInMemoryActivated") ?? false;
+            var logsActivated = _cacheService.Get<bool?>(LoggerResources.TEMP_LOGS_IN_MEMORY_ACTIVATED) ?? false;
             if (!logsActivated) return;
-            var logs = _cacheService.Get<IEnumerable<string>>("TempLogs")?.ToList() ?? new List<string>();
-            logs.Add(message);
-            _cacheService.Set("TempLogs", logs);
-            _cacheService.Set("LogsUpdated", true);
+            var logs = _cacheService.Get<IEnumerable<object>>(LoggerResources.TEMP_LOGS)?.ToList() ?? new List<object>();
+            logs.Add(new
+            {
+                logEvent.Level,
+                Message = message
+            });
+            _cacheService.Set(LoggerResources.TEMP_LOGS, logs);
+            _cacheService.Set(LoggerResources.LOGS_UPDATED, true);
         }
     }
 }
