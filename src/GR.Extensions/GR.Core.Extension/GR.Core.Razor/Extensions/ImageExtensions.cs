91115eac9a083ea60edf377cbb2bba0c307c041e
Lupei Nicolae <nicolae.lupei@indrivo.com>
2020-01-09 19:06:36 +0200

    fix: usage of mono System.Drawing  in corefx,  fix usage in linux env

--- src/GR.Extensions/GR.Core.Extension/GR.Core.Razor/Extensions/ImageExtensions.cs
+++ src/GR.Extensions/GR.Core.Extension/GR.Core.Razor/Extensions/ImageExtensions.cs
@@ -56,8 +56,6 @@ namespace GR.Core.Razor.Extensions
             var destRect = new Rectangle(0, 0, width, height);
             var destImage = new Bitmap(width, height);
 
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
             using (var graphics = Graphics.FromImage(destImage))
             {
                 graphics.CompositingMode = CompositingMode.SourceCopy;
---
+++ src/GR.Extensions/GR.Core.Extension/GR.Core.Razor/README.md
@@ -0,0 +1,8 @@
+# `GR.Core.Razor`
+
+`Note: `This library contains `System.Drawing` namespace, for usage in Linux, you must install the following packages:
+
+```shell
+sudo apt install libc6-dev 
+sudo apt install libgdiplus
+```
