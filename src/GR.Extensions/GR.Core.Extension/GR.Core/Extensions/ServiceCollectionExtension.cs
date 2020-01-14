5a17805ca31d54bc865f9671e48948a56733a98b
Ion Dorosenco <ion.dorosenco@soft-tehnica.com>
2020-01-14 13:12:50 +0200

    edit register

--- src/GR.Extensions/GR.Core.Extension/GR.Core/Extensions/ServiceCollectionExtension.cs
+++ src/GR.Extensions/GR.Core.Extension/GR.Core/Extensions/ServiceCollectionExtension.cs
@@ -66,7 +66,7 @@ namespace GR.Core.Extensions
         /// <returns></returns>
         public static IServiceCollection AddUrlHelper(this IServiceCollection services)
         {
-            services.AddScoped<IUrlHelper>(factory =>
+            services.AddSingleton<IUrlHelper>(factory =>
             {
                 var actionContext = factory.GetService<IActionContextAccessor>()
                     ?.ActionContext;
