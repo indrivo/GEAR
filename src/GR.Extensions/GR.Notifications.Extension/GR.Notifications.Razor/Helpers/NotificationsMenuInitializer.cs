e1fc0a8833b90af3e47f4e6fe1da32046502f92e
Lupei Nicolae <nicolae.lupei@indrivo.com>
2020-03-11 15:32:26 +0200

    feat: add menu initializer for notifications module

---
+++ src/GR.Extensions/GR.Notifications.Extension/GR.Notifications.Razor/Helpers/NotificationsMenuInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GR.Core;
+using GR.UI.Menu.Abstractions.Helpers;
+
+namespace GR.Notifications.Razor.Helpers
+{
+    public class NotificationsMenuInitializer: BaseMenuInitializer
+    {
+        public override MenuInitBuilder Builder => new MenuInitBuilder
+        {
+            MenuGroup = MenuResources.AppMenuId,
+            Configs = new List<MenuItemConfig>
+            {
+                new MenuItemConfig
+                {
+                    Id = NotificationsRazorResources.Menu.Notifications,
+                    Name = "Notifications",
+                    ParentMenuItemId = MenuResources.MenuItems.ConfigurationItem,
+                    Href = MenuResources.MenuItems.None,
+                    Translate = "notifications",
+                    AllowedRoles = new List<string>
+                    {
+                        GlobalResources.Roles.ADMINISTRATOR
+                    }
+                },
+                new MenuItemConfig
+                {
+                    Id = NotificationsRazorResources.Menu.Subscriptions,
+                    Name = "Notifications subscriptions",
+                    ParentMenuItemId = MenuResources.MenuItems.ConfigurationItem,
+                    Href = "/NotificationSubscriptions",
+                    AllowedRoles = new List<string>
+                    {
+                        GlobalResources.Roles.ADMINISTRATOR
+                    }
+                }
+            }
+        };
+    }
+}
