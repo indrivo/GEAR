7433a9478df832cf917d194bf3b847a7ef140646
Lupei Nicolae <nicolae.lupei@indrivo.com>
2020-03-24 14:38:06 +0200

    fix: place of documents in menu

--- src/GR.Extensions/GR.Documents.Extension/GR.Documents.Razor/Helpers/DocumentRazorResources.cs
+++ src/GR.Extensions/GR.Documents.Extension/GR.Documents.Razor/Helpers/DocumentRazorResources.cs
@@ -6,6 +6,7 @@ namespace GR.Documents.Razor.Helpers
     {
         public static class Menu
         {
+            public static Guid DocumentSectionMenuItem = Guid.Parse("acb681ed-d677-48d0-a8f8-b3a0a59207c8");
             public static Guid DocumentMenuItem = Guid.Parse("c373bda8-0d91-48ab-b66a-f5457f2fa3e2");
             public static Guid DocumentTypeMenuItem = Guid.Parse("e85a596f-61a0-4b96-aad3-4826f0eed8a4");
             public static Guid DocumentCategoryMenuItem = Guid.Parse("2fd37f77-40dc-4f69-9339-b1d6af7aa523");
