870b6185e7a61d5ac41f7b49512b732e878bd869
Lupei Nicolae <nicolae.lupei@indrivo.com>
2020-04-06 17:27:27 +0300

    fix: remove unused

--- src/GR.WebHosts/GR.Cms/publish.savecrypto.bat
+++
@@ -1,9 +0,0 @@
-plink -ssh -pw Gear2020 -t nlupei@192.168.1.227 "sudo systemctl stop kestrel-savecrypto.service; sudo rm -r /usr/www/savecrypto; sudo mkdir /usr/www/savecrypto; chmod 777 /usr/www/savecrypto"
-dotnet restore
-dotnet build
-rmdir /Q /S ./dist
-dotnet publish -c Release -o ./dist
-scp -r ./dist/ nlupei@192.168.1.227:/usr/www/savecrypto
-color 2
-echo Deploy with success
-PAUSE
